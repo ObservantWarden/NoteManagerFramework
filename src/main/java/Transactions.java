a3837f5e22ec50ea954813cd37db3d65c48d5650
WeOwnYou <maniachin_2001@mail.ru>
2022-03-31 22:28:38 +0300

    INote finished

--- src/main/java/Transactions.java
+++ src/main/java/Transactions.java
@@ -9,10 +9,10 @@ import java.sql.SQLException;
 public class Transactions {
     public static void sendNote(Connection con, String text) {
         try {
-            ResultSet rs = con.createStatement().executeQuery("select max(id) from pizda;");
+            ResultSet rs = con.createStatement().executeQuery("select max(id) from Bd;");
             rs.next();
             int id = rs.getInt(1) + 1;
-            String command = "INSERT INTO Pizda (id, text) values (" + id + ", '" + text + "');";
+            String command = "INSERT INTO Bd (id, text) values (" + id + ", '" + text + "');";
             con.createStatement().execute(command);
             con.close();
         } catch (SQLException e) {
@@ -22,7 +22,7 @@ public class Transactions {
 
     public static String getNote(Connection con, int id) {
         try {
-            ResultSet rs = con.createStatement().executeQuery("SELECT text  FROM Pizda WHERE id = " + id + ";");
+            ResultSet rs = con.createStatement().executeQuery("SELECT text  FROM Bd WHERE id = " + id + ";");
             rs.next();
             String result = rs.getString(1);
             con.close();
